8f9064359af7efc255bbd6e9d2736594092d1228
Jxse111 <josemartinezestrada111@gmail.com>
2023-03-30 13:20:54 +0200

    •Refactoriza el modelo para que pasa a ser ficheros en vez de memoria, modificando todo lo que se indica en el diagrama de clases.

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/ficheros/IFuenteDatos.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.ficheros;
+
+public interface IFuenteDatos {
+
+	void crearClientes();
+
+	void crearVehiculo();
+
+	void crearAlquileres();
+
+}
