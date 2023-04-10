d7905366e894454385c5c735c4d6f46199e8ecd0
Jxse111 <josemartinezestrada111@gmail.com>
2023-04-10 13:54:48 +0200

    He creado UtilidadesXml y he creado sus tres metodos

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/ficheros/UtilidadesXml.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.ficheros;
+
+public class UtilidadesXml {
+public Object crearCnstructorDocumentoXml() {
+	return DocumentBuilder;
+}
+public void excribirXmlaFichero() {
+	
+}
+public void leerXmlDeFichero() {
+	
+}
+}
