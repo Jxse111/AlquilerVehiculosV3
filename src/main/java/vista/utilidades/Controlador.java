235288c6cde379b252780014e2dc793d82b3070c
Jxse111 <josemartinezestrada111@gmail.com>
2023-04-28 10:48:36 +0200

    .

---
+++ src/main/java/vista/utilidades/Controlador.java
@@ -0,0 +1,20 @@
+package org.iesalandalus.programacion.alquilervehiculos.vista.ventanas.utilidades;
+
+import javafx.stage.Stage;
+
+public class Controlador {
+
+	private Stage escenario;
+	
+	public Stage getEscenario() {
+		return escenario;
+	}
+	
+	public void setEscenario(Stage escenario) {
+		if (escenario == null) {
+			throw new NullPointerException("ERROR: El escenario no puede ser nulo.");
+		}
+		this.escenario = escenario;
+	}
+	
+}
---
+++ src/main/java/vista/utilidades/Controladores.java
@@ -0,0 +1,44 @@
+package org.iesalandalus.programacion.alquilervehiculos.vista.ventanas.utilidades;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.iesalandalus.programacion.alquilervehiculos.vista.ventanas.recursos.LocalizadorRecursos;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Controladores {
+
+	private static final Map<String, Controlador> controladores = new HashMap<>();
+
+	private Controladores() {
+	}
+
+	public static Controlador get(String vistaFxml, String titulo, Stage propietario) {
+		return controladores.getOrDefault(vistaFxml, crear(vistaFxml, titulo, propietario));
+	}
+
+	private static Controlador crear(String vistaFxml, String titulo, Stage propietario) {
+		Controlador controlador = null;
+		try {
+			FXMLLoader cargador = new FXMLLoader(LocalizadorRecursos.class.getResource(vistaFxml));
+			Parent raiz = cargador.load();
+			controlador = cargador.getController();
+			Stage escenario = new Stage();
+			controlador.setEscenario(escenario);
+			escenario.initOwner(propietario);
+			escenario.initModality(Modality.APPLICATION_MODAL);
+			escenario.setTitle(titulo);
+			escenario.setScene(new Scene(raiz));
+			controladores.put(vistaFxml, controlador);
+		} catch (IOException e) {
+			System.out.println("Error al cargar: " + vistaFxml);
+		}
+		return controlador;
+	}
+}
