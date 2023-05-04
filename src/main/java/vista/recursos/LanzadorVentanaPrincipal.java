fdcf512aa064cd639e97de999ee7b9a6d4e5c1aa
Jxse111 <josemartinezestrada111@gmail.com>
2023-05-04 13:38:39 +0200

    .

--- src/main/java/vista/recursos/LanzadorVentanaPrincipal.java
+++ src/main/java/vista/recursos/LanzadorVentanaPrincipal.java
@@ -5,33 +5,37 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import vista.utilidades.Controlador;
 import vista.utilidades.Controladores;
+import vista.utilidades.Dialogos;
 
 public class LanzadorVentanaPrincipal extends Application {
-	
+
 	private static final String TITULO = null;
 
 	@Override
 	public void start(Stage escenarioPrincipal) throws Exception {
 		try {
-			Controlador VentanaPrincipal = Controladores.get("/AlquilerVehiculos-v3/src/main/resources/vistas/VentanaPrincipal.fxml",TITULO, null);
-			VentanaPrincipal.getEscenario().setOnCloseRequest(e -> confirmarSalida(VentanaPrincipal.getEscenario(),e));
-		    VentanaPrincipal.getEscenario().show();
+			Controlador VentanaPrincipal = Controladores.get("/AlquilerVehiculos-v3/src/main/resources/vistas/VentanaPrincipal.fxml", TITULO, null);
+			VentanaPrincipal.getEscenario().setOnCloseRequest(e -> confirmarSalida(VentanaPrincipal.getEscenario(), e));
+			VentanaPrincipal.getEscenario().show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public static void comenzar() {
-	launch(LanzadorVentanaPrincipal.class);
-		
+		launch(LanzadorVentanaPrincipal.class);
+
 	}
 
 	private void confirmarSalida(Stage escenario, WindowEvent e) {
-		
-	
-}
-}
+		if (Dialogos.mostrarDialogoConfirmacion("Salir", "¿Estas seguro que quieres salir?", getEscenario()));
+		getEscenario().close();
+}else
 
+	{
+		e.consume();
+	}
+
+}}
 
 
