4623a5e1794c8a811ff8c8301705c92322b83cb9
Jxse111 <josemartinezestrada111@gmail.com>
2023-05-19 10:30:44 +0200

    .

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/ListarClientes.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/ListarClientes.java
@@ -1,8 +1,14 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import vista.utilidades.Controlador;
 
 public class ListarClientes extends Controlador {
 
+	@FXML
+	void cerrar(ActionEvent event) {
+
+	}
 
 }
